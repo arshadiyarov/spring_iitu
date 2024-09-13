2e0af822f03814bf7fb61e205ea37e3072e7d0c4
arshadiyarov <shadiyarovarmat@gmail.com>
2024-09-13 09:34:27 +0500

    feat: add querry param in /api endpoint

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-  @GetMapping("/hello")
-  public String sayHello() {
-    return "Hello World!";
+  @GetMapping
+  public String sayHello(@RequestParam(name = "q", required = true) int q) {
+    return "Your number " + q;
   }
 }
